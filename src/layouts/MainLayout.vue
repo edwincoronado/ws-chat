3ed05206cb3d7e1f7d65fa93df5ead917e883a44
edwin.coronado <edwin.coronado@servicenow.com>
2020-04-18 15:17:45 -0700

    Commiting inital layout.

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg" />
+          </q-avatar>Title
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="right = !right" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="right" side="right" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <div class="q-pa-md row justify-center">
+        <div style="width: 100%; max-width: 400px">
+          <q-chat-message label="Sunday, 19th" />
+
+          <q-chat-message
+            name="me"
+            avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+            :text="['hey, how are you?']"
+            sent
+            stamp="7 minutes ago"
+          />
+          <q-chat-message
+            name="Jane"
+            avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+            :text="['doing fine, how r you?']"
+            stamp="4 minutes ago"
+          />
+        </div>
+      </div>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      right: false
+    };
+  }
+};
+</script>
